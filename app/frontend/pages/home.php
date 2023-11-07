86d0667cd676be2ef700c9e36253d38c66f2fd73
Y0S3FXD <shekyousef@gmail.com>
2023-11-07 12:37:52 +0100

    Quick Changes

--- app/frontend/pages/home.php
+++ app/frontend/pages/home.php
@@ -1,40 +1,50 @@
-<div class="container" style="margin-top:30px">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Bootstrap CSS -->
+  <link href="bootstrap.min.css" rel="stylesheet">
+  <title>Home Page - Product Showcase</title>
+</head>
+
+<body>
+  <div class="container" style="margin-top:30px">
+    <!-- Products Section -->
+    <!-- Backend Team !!! here is where you will add the products from the database -->
     <div class="row">
-      <div class="col-sm-4">
-        <h2>About Me</h2>
-        <h5>Photo of me:</h5>
-        <div class="fakeimg">Fake Image</div>
-        <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-        <h3>Some Links</h3>
-        <p>Lorem ipsum dolor sit ame.</p>
-        <ul class="nav nav-pills flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">Active</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#">Disabled</a>
-          </li>
-        </ul>
-        <hr class="d-sm-none">
+      <!-- Product 1 -->
+      <div class="col-md-4">
+        <div class="card">
+          <img class="card-img-top" src="placeholder-image-1.jpg" alt="Product 1">
+          <div class="card-body">
+            <h5 class="card-title">Product 1</h5>
+            <p class="card-text">Description for Product 1.</p>
+            <p class="card-text">$19.99</p>
+            <a href="#" class="btn btn-primary">Add to Cart</a>
+          </div>
+        </div>
       </div>
-      <div class="col-sm-8">
-        <h2>TITLE HEADING</h2>
-        <h5>Title description, Dec 7, 2017</h5>
-        <div class="fakeimg">Fake Image</div>
-        <p>Some text..</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-        <br>
-        <h2>TITLE HEADING</h2>
-        <h5>Title description, Sep 2, 2017</h5>
-        <div class="fakeimg">Fake Image</div>
-        <p>Some text..</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <!-- Product 2 -->
+      <div class="col-md-4">
+        <div class="card">
+          <img class="card-img-top" src="https://images.ctfassets.net/ub3bwfd53mwy/5WFv6lEUb1e6kWeP06CLXr/acd328417f24786af98b1750d90813de/4_Image.jpg?w=750" alt="Product 6">
+          <div class="card-body text-center">
+            <h5 class="card-title">Cat</h5>
+            <p class="card-text">Cute Cat</p>
+            <p class="card-text">Not To Sale</p>
+            <a href="#" class="btn btn-primary">Add to Cart</a>
+          </div>
+        </div>
       </div>
     </div>
   </div>
+
+  <!-- Bootstrap JS and dependencies -->
+  <script src="jquery.min.js"></script>
+  <script src="popper.min.js"></script>
+  <script src="bootstrap.min.js"></script>
+</body>
+
+</html>
